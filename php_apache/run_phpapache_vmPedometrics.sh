28f20c3207fab114a6911c057867f936a802f60e
Giuliano Langella <gyuliano@libero.it>
2024-03-17 00:53:53 +0100

    php-apache | generalize image + container all VMs

---
+++ php_apache/run_phpapache_vmPedometrics.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+# DESCRIPTION
+#  This script runs the PHP-APACHE container in Pedometrics VM
+#  on top of an image build using the Dockerfile_phpapache.
+# 
+# REFERENCES
+#   - ...
+#
+# ISSUES
+#  Remove dependence of DB on station mac-address,
+#  which may change over time.
+#
+# CALL
+#  ./run [<IMAGE-NAME>,<CONTAINER-NAME>,<BRIDGE-NAME>]
+
+
+# ========= PARAMETERS =============
+PORT_WEB=8080
+PORT_SSL=444
+DEF_IMAGE_NAME=php_apache_rstudio
+DEF_CONTAINER_NAME=php_apache-booklets
+DEF_NET_NAME=bridge
+# On VM RStudio Didactics on Granatellum proxmox:
+WEBAPPS=/home/giuliano/docker-persistencies/php-rstudio/html
+AP_BOOK=/home/giuliano/AP_book/online
+
+# ========= ARGS =============
+IMAGE_NAME=${1:-$DEF_IMAGE_NAME}
+CONTAINER_NAME=${2:-$DEF_CONTAINER_NAME}
+NET_NAME=${3:-$DEF_NET_NAME}
+
+# ========= START =============
+echo "Docker cycle is:   IMAGE(${IMAGE_NAME}) --> CONTAINER(${CONTAINER_NAME})"
+
+# check container existence and kill it before to run again:
+container_id=$(docker ps -q --filter "name=$CONTAINER_NAME$")
+
+if [ ! -z "$container_id" ]; then
+  echo "  container $CONTAINER_NAME already running..."
+  echo "    stopping it.."
+  docker stop $container_id
+  echo "    removing it..."
+  docker rm $container_id
+else
+  echo "  container $CONTAINER_NAME not existent..."
+fi
+
+
+echo "  Start container $CONTAINER_NAME running on top of $IMAGE_NAME image"
+echo "      > network      : $DEF_NET_NAME"
+echo "      > port http    : $PORT_WEB"
+echo "      > port https   : $PORT_SSL"
+echo "      > main HTML    : $WEBAPPS"
+echo "      > AP book      : $AP_BOOK"
+echo ""
+docker run \
+-d \
+--name $CONTAINER_NAME \
+--restart always \
+--network $NET_NAME \
+-v $WEBAPPS:/var/www/html \
+-v $AP_BOOK:/var/www/html/ap_book \
+-p $PORT_WEB:80 \
+-p $PORT_SSL:443 \
+-t \
+$IMAGE_NAME
+echo ""
+echo "  ...done!"
+echo ""
+echo ""
+
